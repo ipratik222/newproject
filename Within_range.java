cf7d798118765b25b50c97fbd7a1bec9d14021ba
ipratik222 <91131042+ipratik222@users.noreply.github.com>
2021-09-28 16:44:07 +0530

    range program

---
+++ Within_range.java
@@ -0,0 +1,17 @@
+package cop;
+
+public class Within_range {
+
+	public static void main(String[] args) {
+		int num=15;
+		if(num>=10 && num<=90) {
+			System.out.println("number is within range");
+		}
+		else
+			System.out.println("number is not within range");
+			
+			
+
+	}
+
+}
